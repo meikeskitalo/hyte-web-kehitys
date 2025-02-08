e6105e56e3bb35a5485662591f187a05c332519a
Mei <keskitalo.mei@gmail.com>
2025-02-08 21:54:11 +0200

    läksyt tein

--- src/models/entry-model.js
+++ src/models/entry-model.js
@@ -24,10 +24,10 @@ const findEntryById = async (id) => {
 const updateEntry = async (id, mood) => {
   try {
     const query = 'UPDATE DiaryEntries SET mood = ? WHERE entry_id = ?'
-    const [rows] = await promisePool.query(query, [mood, id]);
-    console.log(rows);
+    const result = await promisePool.query(query, [mood, id]);
+    console.log(result);
     // return only first item of the result array
-    return rows[0];
+    return result;
   } catch (error) {
     console.error(error);
     throw new Error('database error');
